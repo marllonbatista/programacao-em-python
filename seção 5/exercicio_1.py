e3c0d991f54600e118ffb30b37c7b6fbc46ce638
Marllon Batista <marllonbatista.12@gmail.com>
2025-08-24 17:20:34 -0300

    atividades da seção 5

---
+++ seção 5/exercicio_1.py
@@ -0,0 +1,7 @@
+'''
+Faça um programa que determine e mostre os cinco primeiros múltiplos de 3,
+considerando números maiores que 0.
+'''
+
+for i in range(1,6):
+    print(i*3)
---
+++ seção 5/exercicio_3.py
@@ -0,0 +1,10 @@
+'''
+Faça um programa que declare um inteiro, inicialize-o com 0, incremente-o de 1000 em 1000,
+imprimindo seu valor na tela, até que seu valor seja 100.000 (cem mil).
+'''
+
+i:int= 0
+print('Contagem regressiva:')
+while i<=100000:
+    print(i)
+    i +=1000
